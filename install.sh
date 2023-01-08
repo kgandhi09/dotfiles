9dddcd6408ab91c85ca0ccaaaadc8df2046b5258
gandhi <kushal.gandhi@verdantrobotics.com>
2023-01-07 23:11:26 -0800

    additions

--- install.sh
+++ install.sh
@@ -7,10 +7,21 @@ cp tmux.conf ~/.tmux.conf
 cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
 cp vimspector.json ~/.vimspector.json
+cp fzf.bash ~/.fzf.bash
+cp fzf.zsh ~/.fzf.zsh
+cp p10k.zsh ~/.p10k.zsh
+touch ~/.bashrc
 
+# Adding zshrc and fzf to bashrc
+ echo 'zsh' >> ~/.bashrc
+ echo '[ -f ~/.fzf.bash ] && source ~/.fzf.bash' >> ~/.bashrc
 
 #Update docker and install dependencies
 sudo apt-get update; sudo apt install software-properties-common; sudo apt-get install python3-apt --reinstall; sudo apt-get install zsh; sudo apt-get install tmux
+
+#Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 #Installing NodeJS (> 12.1 required)
 cd ~/; sudo apt-get update; sudo pip uninstall neovim; sudo pip3 install neovim; curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
@@ -22,3 +33,6 @@ curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --c
 
 #Install ctags
 sudo apt-get update; sudo apt-get install ctags
+
+#Install nvim plugins
+~/nvim.appimage -c ':PlugInstall|:UpdateRemotePlugins|qa!' ~/.config/nvim/init.vim
