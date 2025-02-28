c51648c862d3c53c7135f22db3de90fef1b09bb1
kgandhi09 <kushal@draapmachines.com>
2025-02-28 12:32:11 +0530

    adding installation script

--- install.sh
+++ install.sh
@@ -22,4 +22,76 @@ install_dependencies() {
     fi
 }
 
+# Install Oh-My-Zsh
+install_oh_my_zsh() {
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        echo "Installing Oh-My-Zsh..."
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+    else
+        echo "Oh-My-Zsh already installed. Skipping..."
+    fi
+}
+
+# Install Vim-Plug for Neovim
+install_vim_plug() {
+    if [ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
+        echo "Installing vim-plug for Neovim..."
+        curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    else
+        echo "vim-plug already installed. Skipping..."
+    fi
+}
+
+# Install TPM for tmux
+install_tpm() {
+    if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+        echo "Installing Tmux Plugin Manager (TPM)..."
+        git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+    else
+        echo "TPM already installed. Skipping..."
+    fi
+}
+
+# Symlink configuration files
+setup_symlinks() {
+    echo "Setting up symlinks..."
+    ln -sf "$DOTFILES_DIR/bashrc" "$HOME/.bashrc"
+    ln -sf "$DOTFILES_DIR/bash_aliases" "$HOME/.bash_aliases"
+    ln -sf "$DOTFILES_DIR/zshrc" "$HOME/.zshrc"
+    ln -sf "$DOTFILES_DIR/p10k.zsh" "$HOME/.p10k.zsh"
+    ln -sf "$DOTFILES_DIR/tmux.conf" "$HOME/.tmux.conf"
+    ln -sf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
+    ln -sf "$DOTFILES_DIR/tmux" "$HOME/.tmux"
+}
+
+# Install Neovim plugins
+install_nvim_plugins() {
+    echo "Installing Neovim plugins..."
+    nvim --headless +PlugInstall +qall
+}
+
+# Install Tmux plugins
+install_tmux_plugins() {
+    echo "Installing Tmux plugins..."
+    $HOME/.tmux/plugins/tpm/bin/install_plugins
+}
+
+# Change default shell to Zsh
+set_default_shell() {
+    if [ "$SHELL" != "$(which zsh)" ]; then
+        echo "Changing default shell to Zsh..."
+        chsh -s $(which zsh)
+    fi
+}
+
 install_dependencies
+install_oh_my_zsh
+install_vim_plug
+install_tpm
+setup_symlinks
+install_nvim_plugins
+install_tmux_plugins
+set_default_shell
+
+echo "Installation complete! Restart your shell to apply changes."
